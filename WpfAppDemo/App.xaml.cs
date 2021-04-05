7ec1bd54dc03124d51d396b2005b44b95c469557
BuildTools <unconfigured@null.spigotmc.org>
2021-04-06 01:03:38 +0500

    Добавьте файлы проекта.

---
+++ WpfAppDemo/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfAppDemo.Data;
+
+namespace WpfAppDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        #region Private members
+        private readonly ServiceProvider serviceProvider;
+        #endregion
+
+        #region Constructor
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+
+            services.AddDbContext<ApplicationContext>(options =>
+            {
+                options.UseSqlite("Data Source = UsersAndProfiles.db;foreign keys=true");
+            });
+
+            services.AddSingleton<MainWindow>();
+            serviceProvider = services.BuildServiceProvider();
+        }
+        #endregion
+
+
+        #region Event Handlers
+        private void OnStartup(object s, StartupEventArgs e)
+        {
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
+        #endregion
+    }
+}
